4817fe208192b5bc9cb45e1bd780c37049e64b78
victor.yang <victor.yang@hellosanta.com.tw>
2019-01-03 16:40:32 +0800

    fix js bug and able to set default value

--- js/twzipcode.js
+++ js/twzipcode.js
@@ -12,27 +12,38 @@
         $.each(Drupal.settings.field_addresstw, function (selector) {
           var id = selector.replace("#div","edit");
           var currentData = Drupal.settings.field_addresstw[selector];
+          var county = $('#' + id + '-county').val();
+          var district = $('#' + id + '-district').val();
+          var zipcode = $('#' + id + '-zipcode').val();
 
           $(selector + ' .address_twzipcode').twzipcode({
-            'css':['form-select twcounty','form-select twdistrict','form-text twzipcode'],
+            'css': ['form-select twcounty','form-select twdistrict','form-text twzipcode'],
             'readonly': true,
           });
-
-          if(currentData !=null){
+          if(currentData != ""){
             $(selector + ' .address_twzipcode').twzipcode('set',{
               'county': currentData['county'],
               'district': currentData['district'],
               'zipcode': currentData['zipcode']
             });
           }
+          else if(county != "" && district != "" && zipcode != ""){
+            $(selector + ' .address_twzipcode').twzipcode('set', {
+              'county': county,
+              'district': district,
+              'zipcode': zipcode
+            });
+
+          }
 
           $(selector).on('change',function(){
             var county =  $(this).find('.address_twzipcode').twzipcode('get', 'county');
             var district =  $(this).find('.address_twzipcode').twzipcode('get', 'district');
             var zipcode =  $(this).find('.address_twzipcode').twzipcode('get', 'zipcode');
-            $(this).find('input.edit-county').val(county);
-            $(this).find('input.edit-district').val(district);
-            $(this).find('input.edit-zipcode').val(zipcode);
+            $('#' + id + '-county').val(county);
+            $('#' + id + '-district').val(district);
+            $('#' + id + '-zipcode').val(zipcode);
+            
           })
 
         });
