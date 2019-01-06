6f986fa81bc410ff280272226e3ef584aaaaf022
victor.yang <victor.yang@hellosanta.com.tw>
2019-01-06 19:10:23 +0800

    remove useless validate

--- src/Plugin/Field/FieldWidget/FieldAddresstwWidget.php
+++ src/Plugin/Field/FieldWidget/FieldAddresstwWidget.php
@@ -68,19 +68,4 @@ class FieldAddresstwWidget extends WidgetBase {
 
       return $element;
     }
-
-    /**
-     * Validate the color text field.
-     */
-    public function validate($element, FormStateInterface $form_state) {
-      // $value = $element['#value'];
-      // if (strlen($value) == 0) {
-      //   $form_state->setValueForElement($element, '');
-      //   return;
-      // }
-      // if (!preg_match('/^#([a-f0-9]{6})$/iD', strtolower($value))) {
-      //   $form_state->setError($element, $this->t("Color must be a 6-digit hexadecimal value, suitable for CSS."));
-      // }
-    }
-
   }
